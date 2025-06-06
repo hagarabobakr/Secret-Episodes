d14895cf84b661eddb64fde4246bf9de510d149b
Hager Abobakr <hagarabobakr1@gmail.com>
2025-06-06 15:00:08 +0300

    first commit

---
+++ app/src/main/java/com/jetpack/donutappui/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.jetpack.donutappui.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Black = Color(0xFF000000)
+val LightPink = Color(0xFFFED8DF)
+val BrightPink = Color(0xFFFF7074)
+val SoftPink = Color(0xFFFF9494)
+val White = Color(0xFFFFFFFF)
+val PeachPink = Color(0xFFFFC7D0)
+val BlueGray = Color(0xFFD7E4F6)
+val DarkNavy = Color(0xFF130F26)
